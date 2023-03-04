64ed7c808e9bcf0fc0e89f4c8697c9f8ab566116
Andrea Romanic <romanic.andrea@gmail.com>
2023-03-04 19:55:46 +0100

    [migration] Integrate AngularJS

--- angular-phonecat-hybrid/src/app-ajs/phone-detail/phone-detail.component.ts
+++ angular-phonecat-hybrid/src/app-ajs/phone-detail/phone-detail.component.ts
@@ -1,20 +1,22 @@
 'use strict';
-
 // Register `phoneDetail` component, along with its associated controller and template
-angular.
-  module('phoneDetail').
-  component('phoneDetail', {
-    templateUrl: './phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController(this: any, $routeParams, Phone) {
-        var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone : any) {
+angular.module('phoneDetail').component('phoneDetail', {
+  template: `<ng-include src="'src/app-ajs/phone-detail/phone-detail.template.html'"></ng-include>`,
+  controller: [
+    '$routeParams',
+    'Phone',
+    function PhoneDetailController(this: any, $routeParams, Phone) {
+      var self = this;
+      self.phone = Phone.get(
+        { phoneId: $routeParams.phoneId },
+        function (phone: any) {
           self.setImage(phone.images[0]);
-        });
+        }
+      );
 
-        self.setImage = function setImage(imageUrl: any) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
-  });
+      self.setImage = function setImage(imageUrl: any) {
+        self.mainImageUrl = imageUrl;
+      };
+    },
+  ],
+});
--- angular-phonecat-hybrid/src/app-ajs/phone-list/phone-list.component.ts
+++ angular-phonecat-hybrid/src/app-ajs/phone-list/phone-list.component.ts
@@ -1,14 +1,13 @@
 'use strict';
 
 // Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: './phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(this: any, Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
-  });
+angular.module('phoneList').component('phoneList', {
+  template: `<ng-include src="'src/app-ajs/phone-list/phone-list.template.html'"></ng-include>`,
+  controller: [
+    'Phone',
+    function PhoneListController(this: any, Phone) {
+      this.phones = Phone.query();
+      this.orderProp = 'age';
+    },
+  ],
+});
